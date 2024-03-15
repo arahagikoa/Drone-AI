4e9af2ecaeaad001618224cc2ef0964df5dcf83c
humbleigor <laicikowski@gmail.com>
2024-03-15 21:36:54 +0100

    Detection and moving images

--- Modele/Face recognition/deep-face-serengil.py
+++ Modele/Face recognition/deep-face-serengil.py
@@ -1,23 +1,16 @@
-from deepface import DeepFace
+
 import matplotlib.pyplot as plt
 
 
 
 
+from deepface import DeepFace
+
 img1_path = "./im1.jpg"
 img2_path = "./im2.jpg"
-
-
-img1 = DeepFace.detectFace(img1_path)
-img2 = DeepFace.detectFace(img2_path)
-
-#plt.imshow(img1)
-#plt.imshow(img2)
 
 model_name = 'VGG-Face'
 
 resp = DeepFace.verify(img1_path, img2_path, model_name=model_name)
 
 print(resp)
-
-
--- Modele/Human detection/test.py
+++ Modele/Human detection/test.py
@@ -1,9 +1,41 @@
 from ultralytics import YOLO
-
-import cv2
+import shutil
+import os
 
 model = YOLO("yolov8s.pt")
 
-results = model.predict(source=0, show=True)
+source_folder = "Modele\\Human detection\\photos_to_detect"
+destination_folder = "Modele\\Human detection\\photos_with_detected_objects"
+move_folder = "runs\\detect\\predict"
+remaining_folder = "Modele\\Human detection\\detected_without_frames"
 
-print(results)
+os.makedirs(destination_folder, exist_ok=True)
+os.makedirs(remaining_folder, exist_ok=True)
+
+for filename in os.listdir(source_folder):
+    if filename.endswith(('.jpg', '.jpeg', '.png')):
+        source_image = os.path.join(source_folder, filename)
+        
+        model.predict(
+            source=source_image, 
+            show=True,  
+            save=True,  
+            save_dir=destination_folder,
+            save_txt=False, 
+            save_crop=False,  
+            box=True,  
+            visualize=False 
+        )
+        
+        # Move the detected image
+        detected_image = os.path.join(move_folder, filename)
+        if os.path.exists(detected_image):
+            shutil.move(detected_image, os.path.join(destination_folder, filename))
+
+# Remove the detection folder
+shutil.rmtree(move_folder)
+
+# Move remaining images from photos_to_detect to detected_without_frames
+for filename in os.listdir(source_folder):
+    source_image = os.path.join(source_folder, filename)
+    shutil.move(source_image, os.path.join(remaining_folder, filename))
